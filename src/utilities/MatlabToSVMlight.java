9bce480470c11d8e43b4158b501fc757b4535423
phellwig <paul_at_hellwigs.de>
2015-12-15 11:01:26 +0100

    MatLab2SVM includes sorting and column ignore

--- src/utilities/MatlabToSVMlight.java
+++ src/utilities/MatlabToSVMlight.java
@@ -2,15 +2,18 @@ package utilities;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-
-
+import java.util.HashMap;
+import java.util.List;
 
 import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
 import configuration.Consts;
+import models.Sorter;
 
 
 
@@ -32,28 +35,72 @@ public class MatlabToSVMlight {
 		// Inputfiles
 		// modelpath
 		// Outputfile
-		if (args.length != 2) {
-			System.err.println("Aufruf: java -jar MatlabToSVMlight.jar quelle ziel");
+		boolean ignore1 = false;
+		boolean sort = false;
+		String source;
+		String target;
+		if (args.length < 2) {
+			System.err.println("Aufruf: java -jar MatlabToSVMlight.jar [-sort] [-ignore1] quelle ziel");
 			System.exit(1);
 		}
 		if (args[0].equals("--version")) {
 			System.out.println("Java4Models. Elsevier Health Analytics. Version: " + Consts.version);
 			System.exit(1);
 		}
-		String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
-		System.out.println(timeStamp + " Starte Transformation von " + args[0] + " nach " + args[1]);
+		int source_argNo = 0;
+		if (args[0].equals("-ignore1") || args[1].equals("-ignore1")) {
+			ignore1=true;
+			source_argNo++;
+		}
+		if (args[0].equals("-sort") || args[1].equals("-sort")) {
+			sort=true;
+			source_argNo++;
+		}
+		source=args[source_argNo];
+		target=args[source_argNo+1];
+		String timeStamp;
+		//Sort by X
+		if (sort) {
+				timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+				System.out.println(timeStamp + " Starte Sortieren von " + source);
+				//get path for temp file
+				Path p = Paths.get(source);
+				Path folder = p.getParent();
+				//get id field
+			try {
+				CSVReader reader = new CSVReader(new FileReader(source), ';', '"');
+				String [] firstLine;
+				String[] idfield = {"PID"};
+				if ((firstLine = reader.readNext()) != null) {
+					if (ignore1) idfield[0]=firstLine[1];
+					else idfield[0]=firstLine[0];
+				}
+				reader.close();
+				
+				Sorter sortfile = new Sorter("matlab",source,Consts.csvFlag,idfield,folder.toString());
+				source = sortfile.sortFileByID();
+			} catch (Exception e) {
+				System.err.println("Fehler beim Sortieren von " + source + " im Ordner " + folder.toString());
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+		
+		//Transform
+		timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+		System.out.println(timeStamp + " Starte Transformation von " + source + " nach " + target);
 		try {
-			transform(args[0],args[1]);
+			transform(source,target,ignore1);
 		} catch (Exception e) {
-			System.err.println("Fehler beim Schreiben oder Lesen von " + args[0] + " oder " + args[1] + ".");
+			System.err.println("Fehler beim Schreiben oder Lesen von " + source + " oder " + target + ".");
 			e.printStackTrace();
 			System.exit(1);
 		}
 		timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
-		System.out.println(timeStamp + " Transformation beendet von " + args[0] + " nach " + args[1]);
+		System.out.println(timeStamp + " Transformation beendet von " + source + " nach " + target);
 	}
 	
-	private static void transform(String readfile, String writefile) throws Exception {
+	private static void transform(String readfile, String writefile, boolean ignore1) throws Exception {
 		CSVReader reader = null;
 		CSVWriter writer = null;
 		reader = new CSVReader(new FileReader(readfile), ';', '"');
@@ -65,17 +112,25 @@ public class MatlabToSVMlight {
 		String currentid= null;
 		ArrayList<Column> profValues = new ArrayList<Column>();
 		Column col;
+		int col_x = 0;
+		int col_y = 1;
+		int col_z = 2;
+		if (ignore1) {
+			col_x = 1;
+			col_y = 2;
+			col_z = 3;
+		}
 		while ((row = reader.readNext()) != null) {
-			if (currentid == null || row[0].equals(currentid)) {
-				col = new Column(row[1],row[2]);
+			if (currentid == null || row[col_x].equals(currentid)) {
+				col = new Column(row[col_y],row[col_z]);
 				profValues.add(col);
 			} else {
 				writeSvmlighRow(writer,profValues,currentid);
 				profValues = new ArrayList<Column>();
-				col = new Column(row[1],row[2]);
+				col = new Column(row[col_y],row[col_z]);
 				profValues.add(col);
 			}
-			currentid=row[0];
+			currentid=row[col_x];
 		}
 		reader.close();
 		writer.close();
